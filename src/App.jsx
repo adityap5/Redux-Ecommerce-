788b026a6480cc50467bb121c1758e4e20f2af34
adityap5 <aditya29pippal@gmail.com>
2024-03-27 00:22:15 +0530

    #Changed_layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
-import FeaturedProducts from './components/FeaturedProducts';
-import Categories from './components/Categories';
-import Weekly from './components/Weekly';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
+import LocomotiveScroll from 'locomotive-scroll';
 function App() {
- 
+  const scroll = new LocomotiveScroll();
 
   return (
     <>
-    <FeaturedProducts/>
-    <Categories/>
-    <Weekly/>
+    <Header/>
+    <Outlet />
     <div><h3 className='text-white uppercase font-bebas tracking-widest text-2xl'>free delivery + return over Rs2000 collecct from store</h3></div>
+    <Footer />
     </>
   )
 }
